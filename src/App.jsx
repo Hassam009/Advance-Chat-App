2537614644a60f714115d45271a1045b27436fcf
Hassam Amer <hasideveloper@gmail.com>
2024-04-30 01:20:32 +0500

    Now imported pages dynamically by using lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { Home } from '@mui/icons-material'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { lazy } from 'react'
+
+const Home=lazy(()=>{
+  import("./Pages/Home")
+})
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>}></Route>
+        <Route path='/' element={Home}></Route>
         <Route path='/About' element={<h1>About</h1>}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  ) 
 }
 
 export default App
+ 
