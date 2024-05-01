f6bfe8b784e9c792e468547d966fbef5c8be7c1d
Hassam Amer <hasideveloper@gmail.com>
2024-05-01 22:13:35 +0500

    Now signup page and login page is ready

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import { Container, Paper, Button, Typography, TextField } from "@mui/material";
 
 const Login = () => {
   const [isLogin, setisLogin] = useState(true); // Make sure to initialize the state correctly
-  const toggleLogin = () => setisLogin(!isLogin);
+  const toggleLogin = () => setisLogin((prev=>!prev));
 
   return (
     <Container component={"main"} maxWidth="xs" 
@@ -11,9 +11,11 @@ const Login = () => {
       height: '100vh',
       display: 'flex', 
       alignItems: 'center',
-      justifyContent: 'center' }}
+      justifyContent: 'center', 
+       // Fixed padding property
+    }}
     >
-      <Paper elevation={3} sx={{ Padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Paper elevation={3} sx={{padding: 3,  display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {isLogin ? (
           <>
             <Typography variant="h5">Login</Typography>
@@ -46,11 +48,59 @@ const Login = () => {
                 sx={{ marginTop: "1rem", justifyContent: 'center' }}
                 variant="text" color="secondary"
                 onClick={toggleLogin}
-              >Sign in</Button>
+              >Sign Up instead</Button>
             </form>
           </> 
         ) : (
-          <span>Register</span>
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form 
+            style={{
+              fontWeight: 'bold',
+              width: '100%',
+              marginTop: '1rem',
+              textAlign: 'center', // Center the form content
+            }}
+            >
+              <TextField
+                required
+                fullWidth
+                label=" Name"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label=" Bio"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="UserName"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button sx={{ marginTop: "1rem", justifyContent: 'center' }} variant="contained" color="primary"
+               type="submit">Sign Up</Button>
+              <Typography textAlign="center" margin="1rem"> OR</Typography>
+              <Button
+                sx={{ marginTop: "1rem", justifyContent: 'center' }}
+                variant="text" color="secondary"
+                onClick={toggleLogin}
+              >Login instead</Button>
+            </form>
+          </> 
         )}
       </Paper>
     </Container>
