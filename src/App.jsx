91458459b13baf84c14904ce2701cab3f06e0d96
Hassam Amer <hasideveloper@gmail.com>
2024-05-06 00:19:31 +0500

    Some page completed and Now working on Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import ProtectRoute from './Components/Styles/auth/ProtectRoute';
 
 const Home = lazy(() => import("./Pages/Home"));
 const Login = lazy(() => import("./Pages/Login"));
 const Chat = lazy(() => import("./Pages/Chat"));
 const Groups = lazy(() => import("./Pages/Groups"));
+const NotFound = lazy(() => import("./Pages/NotFound"));
+
+let user=true;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+          <Route  element={<ProtectRoute user={user}/>}>
           <Route path='/' element={<Home />} />
           <Route path='/chat' element={<Chat />} />
           <Route path='/groups' element={<Groups />} />
-          <Route path='/login' element={<Login />} />
+          </Route>
+
+       <Route path='/login' 
+          element={
+          <ProtectRoute user={!user}
+          redirect='/'>
+          <Login/>
+          </ProtectRoute>
+        } 
+          />
+          <Route path='*'>{< NotFound/>}</Route>
+
         </Routes>
       </Suspense>
     </BrowserRouter>
---
+++ src/Components/Styles/auth/ProtectRoute.jsx
@@ -0,0 +1,10 @@
+
+import { Navigate, Outlet } from "react-router-dom"
+
+const ProtectRoute = ({children, user, redirect="/login"}) => {
+ if(!user) return <Navigate to={redirect}></Navigate>
+ 
+    return children? children:<Outlet></Outlet> ;
+}
+
+export default ProtectRoute
